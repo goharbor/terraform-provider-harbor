72d26727ca21a33064da9481210a111736f4f479
Brett Wright <brett.wright@bestseller.com>
2020-08-26 16:56:02 +0200

    Removed commented out code

--- client/user.go
+++ client/user.go
@@ -18,38 +18,3 @@ func UserBody(d *schema.ResourceData) models.UserBody {
 		Newpassword:  d.Get("password").(string),
 	}
 }
-
-// CreateUser will create an interal user
-// func (client *Client) CreateUser(body *models.UserBody) (*models.UserBody, error) {
-// 	path := models.PathUsers
-
-// 	resp, err := client.SendRequest("POST", path, body, 201)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	var jsonData models.UserBody
-// 	err = json.Unmarshal([]byte(resp), &jsonData)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &jsonData, nil
-// }
-
-// UpdateUser will update an Interal user
-// func (client *Client) UpdateUser(id string, d *schema.ResourceData) (json string, error) {
-
-// 	body := models.UserBody{
-// 		Password:     d.Get("password").(string),
-// 		Email:        d.Get("email").(string),
-// 		Realname:     d.Get("full_name").(string),
-// 	}
-
-// 	_, err := client.SendRequest("PUT", id, updatebody, 200)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return nil, nil
-// }
