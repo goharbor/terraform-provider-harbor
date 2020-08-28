e261f1b3d2671da59b283ad676993bc5c69099ed
Brett Wright <brett.wright@bestseller.com>
2020-08-28 10:21:54 +0200

    refactored robot accounts

---
+++ models/robot_account.go
@@ -0,0 +1,21 @@
+package models
+
+type RobotBody struct {
+	Access      []RobotBodyAccess `json:"access,omitempty"`
+	Name        string            `json:"name,omitempty"`
+	ExpiresAt   int               `json:"expires_at,omitempty"`
+	Description string            `json:"description,omitempty"`
+}
+type RobotBodyAccess struct {
+	Action   string `json:"action,omitempty"`
+	Resource string `json:"resource,omitempty"`
+}
+type RobotBodyRepones struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Token       string `json:"token"`
+	Description string `json:"description"`
+	ProjectID   int    `json:"project_id"`
+	ExpiresAt   int    `json:"expires_at"`
+	Disabled    bool   `json:"disabled"`
+}
