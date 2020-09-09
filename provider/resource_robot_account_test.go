483cdb6ea259452a25e424d65b5bbda2d1fe05a3
Brett Wright <brett.wright@bestseller.com>
2020-09-09 10:35:01 +0200

    Added acceptance testing

---
+++ provider/resource_robot_account_test.go
@@ -0,0 +1,67 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/BESTSELLER/terraform-provider-harbor/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccRobotBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRobotDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckRobotBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceExists("harbor_project.main"),
+
+					testAccCheckResourceExists("harbor_robot_account.main"),
+					resource.TestCheckResourceAttr(
+						"harbor_robot_account.main", "name", "test_robot_account"),
+					resource.TestCheckResourceAttr(
+						"harbor_robot_account.main", "action", "push"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckRobotDestroy(s *terraform.State) error {
+	apiClient := testAccProvider.Meta().(*client.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "harbor_robot_account" {
+			continue
+		}
+
+		resp, _, err := apiClient.SendRequest("GET", rs.Primary.ID, nil, 404)
+		if err != nil {
+			return fmt.Errorf("Resouse was not delete \n %s", resp)
+		}
+
+	}
+
+	return nil
+}
+
+func testAccCheckRobotBasic() string {
+	return fmt.Sprintf(`
+
+	resource "harbor_robot_account" "main" {
+		name        = "test_robot_account"
+		description = "Robot account to be used to push images"
+		project_id  = harbor_project.main.id
+		action      = "push"
+	  }
+
+	  resource "harbor_project" "main" {
+		name = "test_basic"
+	  }
+	  
+	`)
+}
