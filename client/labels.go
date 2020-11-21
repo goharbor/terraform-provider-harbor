8213d9200598241fbbe15374818945cbd9f77284
Brett Wright <brettwright@pop-os.localdomain>
2020-11-21 18:29:16 +0100

    change the project id input

--- client/labels.go
+++ client/labels.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/BESTSELLER/terraform-provider-harbor/models"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -13,19 +16,13 @@ func LabelsBody(d *schema.ResourceData) models.Labels {
 	}
 
 	if _, ok := d.GetOk("project_id"); ok {
+		id := d.Get("project_id").(string)
 
-		body.ID = d.Get("project_id").(int)
+		body.ID, _ = strconv.Atoi(strings.ReplaceAll(id, "/projects/", ""))
 		body.Scope = "p"
 	} else {
+
 		body.Scope = "g"
 	}
 	return body
 }
-
-// ) models.ProjectsBodyPost {
-// 	body := models.ProjectsBodyPost{
-// 		ProjectName: d.Get("name").(string),
-// 	}
-// 	body.Metadata.AutoScan = d.Get("vulnerability_scanning").(string)
-// 	body.Metadata.Public = d.Get("public").(string)
-// 	return body
