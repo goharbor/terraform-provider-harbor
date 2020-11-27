c25c9236aa64eac1753fe104877c25eea0987f27
Brett Wright <brett.wright@bestseller.com>
2020-11-27 07:46:24 +0100

    updated to sdk v2

--- provider/resource_retention_policy.go
+++ provider/resource_retention_policy.go
@@ -3,7 +3,7 @@ package provider
 import (
 	"github.com/BESTSELLER/terraform-provider-harbor/client"
 	"github.com/BESTSELLER/terraform-provider-harbor/models"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceRentention() *schema.Resource {
