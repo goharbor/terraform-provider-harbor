a3b8526ade33ff7ee3c8cee3f4a4d1faae082f16
Brett Wright <brett.wright@bestseller.com>
2021-04-15 13:17:25 +0200

    Created test for garbage collection

---
+++ provider/resource_garbage_collection_test.go
@@ -0,0 +1,80 @@
+// +build external_auth
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/BESTSELLER/terraform-provider-harbor/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+const harborGCMain = "harbor_garbage_collection.main"
+
+func testAccCheckGCDestroy(s *terraform.State) error {
+	apiClient := testAccProvider.Meta().(*client.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "harbor_garbage_collection" {
+			continue
+		}
+
+		resp, _, err := apiClient.SendRequest("GET", rs.Primary.ID, nil, 200)
+		if err != nil {
+			return fmt.Errorf("Resouse was not delete \n %s", resp)
+		}
+		if resp != "" {
+			return fmt.Errorf("Resouse was not delete \n %s", resp)
+		}
+
+	}
+
+	return nil
+}
+
+func TestAccGCUpdate(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGCDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckGCBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceExists(harborGCMain),
+					resource.TestCheckResourceAttr(
+						harborGCMain, "schedule", "daily"),
+				),
+			},
+			{
+				Config: testAccCheckGCUpdate(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceExists(harborGCMain),
+					resource.TestCheckResourceAttr(
+						harborGCMain, "schedule", "hourly"),
+					resource.TestCheckResourceAttr(
+						harborGCMain, "delete_untagged", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGCBasic() string {
+	return fmt.Sprintf(`
+	resource "harbor_garbage_collection" "main" {
+		schedule        = "daily"
+	}
+	`)
+}
+
+func testAccCheckGCUpdate() string {
+	return fmt.Sprintf(`
+	resource "harbor_garbage_collection" "main" {
+		schedule        = "hourly"
+		delete_untagged = true
+	}
+	`)
+}
