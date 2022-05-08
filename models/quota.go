eba5bb679b2f1fc2cd7616891580be782ab10c93
Hai Wu <hai@hudson-trading.com>
2022-05-08 15:34:06 -0400

    fix harbor project quota update bug

--- models/quota.go
+++ models/quota.go
@@ -1,8 +1,25 @@
 package models
 
+import "time"
+
 type StorageQuota struct {
 	Hard Hard `json:"hard"`
 }
 type Hard struct {
 	Storage int64 `json:"storage"`
+}
+type Used struct {
+	Storage int64 `json:"storage"`
+}
+type QuotaResponse struct {
+	CreationTime	time.Time	`json:"creation_time"`
+	Hard		Hard		`json:"hard"`
+	ID		int		`json:"id"`
+	Ref struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Owner string `json:"owner"`
+        } `json:"ref"`
+	UpdateTime	time.Time	`json:"update_time"`
+	Used		Used		`json:"used"`
 }
