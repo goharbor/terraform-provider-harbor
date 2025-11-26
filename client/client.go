goharbor/terraform-provider-harbor

commit 5248a3a2d4ac9ec990fced4b04f9fb4ede45cd8b

improve forbiden message error (fixes #528) (#533)
improve forbiden message error (fixes #528)

Signed-off-by: flbla <flbla@users.noreply.github.com>

client/client.go
@@ -103,18 +103,21 @@ func (c *Client) SendRequest(method string, path string, payload interface{}, st
 	}
 
 	if resp.StatusCode == http.StatusForbidden {
-		csrfheaders, err := extractCsrfHeaders(resp.Header)
-		if err != nil {
-			return "", "", resp.StatusCode, err
-		}
-		resp, err = c.sendRequestWithHeaders(method, path, payload, csrfheaders)
-		if err != nil {
-			if resp != nil {
+		if c.sessionId != "" {
+			csrfheaders, err := extractCsrfHeaders(resp.Header)
+			if err != nil {
 				return "", "", resp.StatusCode, err
-			} else {
-				return "", "", http.StatusBadGateway, err
+			}
+			resp, err = c.sendRequestWithHeaders(method, path, payload, csrfheaders)
+			if err != nil {
+				if resp != nil {
+					return "", "", resp.StatusCode, err
+				} else {
+					return "", "", http.StatusBadGateway, err
+				}
 			}
 		}
+		return "", "", resp.StatusCode, fmt.Errorf("[ERROR] forbidden: status=%s, code=%d \nIf you are using a robot account, this is likely due to RBAC limitations. See: https://github.com/goharbor/community/blob/main/proposals/new/Robot-Account-Expand.md", resp.Status, resp.StatusCode)
 	}
 
 	body, err := io.ReadAll(resp.Body)

provider/resource_config_auth.go
@@ -197,7 +197,9 @@ func resourceConfigAuthRead(d *schema.ResourceData, m interface{}) error {
 	resp, _, respCode, err := apiClient.SendRequest("GET", models.PathConfig, nil, 200)
 	if respCode == 404 && err != nil {
 		d.SetId("")
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
+	} else if err != nil {
+		return err
 	}
 
 	err = client.SetAuthValues(d, resp)

provider/resource_config_security.go
@@ -61,6 +61,8 @@ func resourceConfigSecurityRead(d *schema.ResourceData, m interface{}) error {
 	if respCode == 404 && err != nil {
 		d.SetId("")
 		return fmt.Errorf("resource not found %s", models.PathSystemCVEAllowList)
+	} else if err != nil {
+		return err
 	}
 
 	var jsonData models.SystemCveAllowListBodyPost

provider/resource_config_system.go
@@ -131,6 +131,8 @@ func resourceConfigSystemRead(d *schema.ResourceData, m interface{}) error {
 	if respCode == 404 && err != nil {
 		d.SetId("")
 		return fmt.Errorf("error getting system configuration %s", err)
+	} else if err != nil {
+		return err
 	}
 
 	var jsonData models.ConfigBodyResponse

provider/resource_garbage_collection.go
@@ -57,7 +57,9 @@ func resourceGCRead(d *schema.ResourceData, m interface{}) error {
 	resp, _, respCode, err := apiClient.SendRequest("GET", models.PathGC, nil, 200)
 	if respCode == 404 && err != nil {
 		d.SetId("")
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
+	} else if err != nil {
+		return err
 	}
 
 	var jsonData models.SystemBody

provider/resource_group.go
@@ -62,13 +62,13 @@ func resourceGroupRead(d *schema.ResourceData, m interface{}) error {
 		d.SetId("")
 		return nil
 	} else if err != nil {
-		return fmt.Errorf("resource not found %s", d.Id())
+		return err
 	}
 
 	var jsonData models.GroupBody
 	err = json.Unmarshal([]byte(resp), &jsonData)
 	if err != nil {
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
 	}
 
 	d.Set("group_name", jsonData.Groupname)

provider/resource_harbor_project_webhook.go
@@ -77,7 +77,7 @@ func resourceProjectWebhookCreate(d *schema.ResourceData, m interface{}) error {
 		return err
 	}
 
-	id, err := client.GetID(headers)
+	id, _ := client.GetID(headers)
 	d.SetId(id)
 	return resourceProjectWebhookRead(d, m)
 }
@@ -88,13 +88,15 @@ func resourceProjectWebhookRead(d *schema.ResourceData, m interface{}) error {
 	resp, _, respCode, err := apiClient.SendRequest("GET", d.Id(), nil, 200)
 	if respCode == 404 && err != nil {
 		d.SetId("")
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
+	} else if err != nil {
+		return err
 	}
 
 	var jsonData models.ProjectWebhook
 	err = json.Unmarshal([]byte(resp), &jsonData)
 	if err != nil {
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
 	}
 
 	d.Set("name", jsonData.Name)

provider/resource_interrogation_services.go
@@ -51,13 +51,15 @@ func resourceVulnRead(d *schema.ResourceData, m interface{}) error {
 
 	if respCode == 404 && err != nil {
 		d.SetId("")
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
+	} else if err != nil {
+		return err
 	}
 
 	var jsonData models.InterogationsBodyResponse
 	err = json.Unmarshal([]byte(resp), &jsonData)
 	if err != nil {
-		return fmt.Errorf("Resource not found %s", d.Id())
+		return fmt.Errorf("resource not found %s", d.Id())
 	}
 
 	vulnerability_scan_policy := jsonData.Schedule.Type

commit 67cfed56d75b0f47f2ad7d14eb52fc3345f4270c

check twice for forbidden before return (fixes #554) (#555)
https://github.com/goharbor/terraform-provider-harbor/commit/5248a3a2d4ac9ec990fced4b04f9fb4ede45cd8b
introduced better error handling for FORBIDDEN errors.
This caused a slight regression in the authentication handling for OIDC
logins (introduced in #497).

This PR moves the error message outside of the "retry" block.

Signed-off-by: Sebastian Höffner <info@sebastian-hoeffner.de>

client/client.go
@@ -117,6 +117,9 @@ func (c *Client) SendRequest(method string, path string, payload interface{}, st
 				}
 			}
 		}
+	}
+
+	if resp.StatusCode == http.StatusForbidden {
 		return "", "", resp.StatusCode, fmt.Errorf("[ERROR] forbidden: status=%s, code=%d \nIf you are using a robot account, this is likely due to RBAC limitations. See: https://github.com/goharbor/community/blob/main/proposals/new/Robot-Account-Expand.md", resp.Status, resp.StatusCode)
 	}
 